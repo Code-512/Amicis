22c1adf5772efdcec6c9502106e02d6a3728a0a5
Jared Harris <jared.harris1@g.austincc.edu>
2019-01-20 16:22:14 -0600

    committing before pulling bo's

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 
-app.get('/login', (req, res)=> {
+router.get('/login', (req, res)=> {
     console.log("Finding details");
     console.log(req.query);
     models.User.find(req.query).then(results => {
