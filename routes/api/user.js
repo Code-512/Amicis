a088bf0a7e122bc546a1e76857ed021f9dc9e006
Jared Harris <jared.harris1@g.austincc.edu>
2019-01-21 16:52:19 -0600

    adding controller for the login button

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,11 +1,16 @@
 const router = require("express").Router();
+const userController = require("../../controllers/usersController");
 
-router.get('/', (req, res)=> {
-    console.log("Finding details");
-    console.log(req.query);
-    models.User.find(req.query).then(results => {
-    res.send(results)
-    })
-})
+// router.route('/', (req, res)=> {
+//     console.log("Finding details");
+//     console.log(req.query);
+//     models.User.find(req.query).then(results => {
+//     res.send(results)
+//     })
+// })
+
+router.route('/')
+    .get(userController.findAll);
+    
 
 module.exports = router;
