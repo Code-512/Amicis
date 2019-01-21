ecf852dbd093226a78069d3339a7344b8bb7413a
aexes3 <41274613+aexes3@users.noreply.github.com>
2019-01-21 15:10:20 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const apiRoutes = require("./api");
 
 // API Routes
-router.use("api/user/login", apiRoutes);
+router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
