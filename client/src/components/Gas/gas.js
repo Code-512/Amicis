c183657838042272756924dbb0d8eb52c47931f9
Wilmer <julieannewilmer@gmail.com>
2019-01-22 19:27:45 -0600

    cleaned up comments in map, gas, air

--- client/src/components/Gas/gas.js
+++ client/src/components/Gas/gas.js
@@ -32,8 +32,6 @@ class Gas extends React.Component {
     });
   }
     
-
-
   render() {
       return (
         <div>
@@ -58,92 +56,3 @@ class Gas extends React.Component {
 }    
 
 export default Gas;
-
-
-
-/// SANDBOX  
-
-// import React from 'react';
-// import axios from 'axios'
-
-// class Gas extends React.Component {
-//     state = {
-//         gaslist: ''
-//     };
-//     componentDidMount(){
-//       console.log('hello from gas api');  
-      
-//       function buildGasURL(lat, lng) {
-
-//             lat = 30.394217
-//             lng = -97.833953
-            
-//               //lat = this.props.lat
-//               // lgn = this.props.lng
-
-//               axios.get('http://api.mygasfeed.com/stations/radius/' + lat + "/" + long + "/5/reg/price/esmbi7wobr.json?')
-//               .then( (data) => {
-//                 // handle success
-//                 console.log("data from gas api", data);
-//                     this.setState({
-//                             gaslist: data
-//                         })
-                        
-//               })
-//               .catch( (error) =>{
-//                 // handle error
-//                 console.log(error);
-//               })
-//               .then( ()=> {
-//                 // always executed
-//                 console.log("All done from gas api")
-//               });
-            
-
-
-//             render() {
-//                 return (
-//                   <div>GAS</div>
-
-
-
-//                 );
-//               }
-//             }
-//           }
-//         }                
-
-// export default Gas;
-
-// ?????????????????????????????????
-
-
-// import React from 'react';
-// import axios from 'axios'
-
-// class Gas extends React.Component {
-    
-//       state = {
-//         persons: []
-//       }
-    
-//       componentDidMount() {
-//         axios.get(`https://jsonplaceholder.typicode.com/users`)
-//           .then(res => {
-//             const persons = res.data;
-//             this.setState({ persons });
-//           })
-         
-//       }
-    
-//       render() {
-//         return (
-//             <div>HI</div>
-//           <ul>
-//             { this.state.persons.map(person => <li>{person.name}</li>)}
-//           </ul>
-//         )
-//       }
-//     }
-
-// export default Gas;
--- client/src/components/Map/map.js
+++ client/src/components/Map/map.js
@@ -1,28 +1,3 @@
-// import React, { Component } from 'react';
-// import { withGoogleMap, GoogleMap } from 'react-google-maps';
-
-// class Map extends Component {
-//    render() {
-//    const GoogleMapExample = withGoogleMap(props => (
-//       <GoogleMap
-//         defaultCenter = { { lat: 40.756795, lng: -73.954298 } }
-//         defaultZoom = { 13 }
-//       >
-//       </GoogleMap>
-//    ));
-//    return(
-//       <div>
-//         <GoogleMapExample
-//           containerElement={ <div style={{ height: `500px`, width: '500px' }} /> }
-//           mapElement={ <div style={{ height: `100%` }} /> }
-//         />
-//       </div>
-//    );
-//    }
-// };
-// export default Map;
-
-// new code: 
 /*global google*/
 import React from 'react'
 import  { compose, withProps, lifecycle } from 'recompose'
