3848700cb99a7f7a77a0b5fcf1404bade86d5061
Wilmer <julieannewilmer@gmail.com>
2019-01-26 13:55:03 -0600

    adjusting drilling down into org and destination to send through link to results page

--- client/src/pages/PlanTrip.js
+++ client/src/pages/PlanTrip.js
@@ -9,76 +9,69 @@ import DatePickTest from './datePickerTest'
 
 class PlanTrip extends Component {
   state = {
-    origin: null,
-    destination: null,
+    originLat: null,
+    originLng: null,
+    originCity: null,
+    destinationLat: null,
+    destinationLng: null,
+    destinationCity: null,
     value: '',
     selectedDate: new Date()
-    
-};
 
-updateLocation = (place) => {
-  console.log('PLACE', place)
-  this.setState({
-    origin: place,
-    destination: place
-  });
-  console.log('update location', this.state);
-};
+  };
 
-handleDateChange = date => {
-  this.setState({ selectedDate: date });
-};
+  updateLocation = (place) => {
+    console.log('PLACE', place)
+    this.setState({
+      originLat: place.geometry.viewport.ga.j,
+    originLng: null,
+    originCity: null,
+    destinationLat: null,
+    destinationLng: null,
+    destinationCity: null,
+    });
+    console.log('update location', this.state);
+  };
 
-formatDate = () => {
-let stringDate = `${this.state.selectedDate.getDate().toString()}/${(this.state.selectedDate.getMonth() +1).toString()}/${(this.state.selectedDate.getFullYear().toString())}`;
-console.log(stringDate)
+  handleDateChange = date => {
+    this.setState({ selectedDate: date });
+  };
 
-return stringDate;
-}
+  formatDate = () => {
+    let stringDate = `${this.state.selectedDate.getDate().toString()}/${(this.state.selectedDate.getMonth() + 1).toString()}/${(this.state.selectedDate.getFullYear().toString())}`;
+    console.log(stringDate)
+
+    return stringDate;
+  }
 
   render() {
-    // this.formatDate();
     console.log('cool')
     return (
       <Container fluid>
-            <Jumbotron>
-              <h1>THIS WILL BE PLAN TRIP PAGE</h1>
-              
-            </Jumbotron>
-      <DatePickTest 
-      handleDateChange={this.handleDateChange} 
-      selectedDate={this.state.selectedDate}
-      />
-      
-
-            {/* <PlacesWithStandaloneSearchBox getOrigin={this.updateLocation}/>
-            <PlacesWithStandaloneSearchBox getDestination={this.updateLocation}/> */}
-            <PlacesWithStandaloneSearchBox 
-            getOrigin={this.updateLocation}
-            onLocationChange={this.updateLocation} 
-            placeholderText="Where ya startin'?" />
-            <PlacesWithStandaloneSearchBox 
-            getDestination={this.updateLocation}
-            onLocationChange={this.updateLocation} 
-            placeholderText="Where ya goin'?" />
-
-            {/* {this.state.origin && 'origin:' + this.state.origin.formatted_address} */}
-            {/* {this.state.destination && 'destination:' + this.state.destination.formatted_address} */}
-            
-            {/* <Link to={`/resuts?origin=${this.state.origin}&dest=${this.state.dest}`}>See Results<Link> */}
-       
-            <Link to={`/results?origin=${this.state.origin}&destination=${this.state.destination}&date=${this.formatDate()}`}
-            // state={
-
-            // }
-            >
-                     <button>plan trip</button>
-            </Link>
+        <Jumbotron>
+          <h1>THIS WILL BE PLAN TRIP PAGE</h1>
+        </Jumbotron>
+        <DatePickTest
+          handleDateChange={this.handleDateChange}
+          selectedDate={this.state.selectedDate}
+        />
+        <PlacesWithStandaloneSearchBox
+          getOrigin={this.updateLocation}
+          onLocationChange={this.updateLocation}
+          placeholderText="Where ya startin'?" />
+        <PlacesWithStandaloneSearchBox
+          getDestination={this.updateLocation}
+          onLocationChange={this.updateLocation}
+          placeholderText="Where ya goin'?" />
+        <Link to={`/results?origin=${this.state.originLat}&destination=${this.state.destination}$&date=${this.formatDate()}`}
+        >
+          <button>plan trip</button>
+        </Link>
       </Container>
-      
-    );  
+
+    );
   }
 }
 
-  
+
 export default PlanTrip;
