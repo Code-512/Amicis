f7941836092c2bb7ca48f4be8ded685ce7d36459
Wilmer <julieannewilmer@gmail.com>
2019-01-26 22:37:14 -0600

    passing props from map, not successful yet

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -6,48 +6,64 @@ import Gas from "../components/Gas/gas";
 import Air from "../components/Air/air";
 
 class Results extends Component {
-  // state = {
-
-  // }
-
-
-
-
-
-
+  state = {
+    date: ""
+    // originLat:'',
+    // originLgn:'',
+    // originCity:'',
+    // destinationLat:'',
+    // destinationLng:'',
+    // destinationCity:''
+  };
 
   componentDidMount() {
-    // Now we must save the data into state and figure out how to pass it to where we need it 
+    // Now we must save the data into state and figure out how to pass it to where we need it
     console.log(this.props.location.search);
     const newParams = new URLSearchParams(this.props.location.search);
-    const origin = newParams.get('origin');
-    const destination = newParams.get('destination');
-    const date = newParams.get('date');
-    console.log(date);
-    console.log(origin)
-    console.log(destination) 
-    console.log("window:", window.location.search)
-    console.log('date from url', window.location.search.date)
+    // const originLat = newParams.get('originLat');
+    // const originLgn = newParams.get('originLng');
+    // const originCity = newParams.get('originCity');
+    // const destinationLat = newParams.get('destinationLat');
+    // const destinationLng = newParams.get('destinationLng');
+    // const destinationCity = newParams.get('destinationCity');
+    const date = newParams.get("date");
+    console.log("date from results page", date);
+    // console.log('originLat', originLat)
+    // console.log(destination)
+    console.log("window:", window.location.search);
+
+    this.setState({
+      date,
+      // originLat,
+      // originLgn,
+      // originCity,
+      // destinationLat,
+      // destinationLng,
+      // destinationCity
+    });
   }
-  
+
   render() {
     return (
-    <Container fluid>
-      <Jumbotron>
-        <h1>Results</h1>
-      </Jumbotron>
-      <Map />
-      <Row>
-        <Col size="md-6">
-          <Gas lat={30.2672} lng={-97.7431} />
-        </Col>
-      <Col size="md-6 sm-12">
-          <Air  />
-        </Col>
-      </Row>
-    </Container>
-    )
+      <Container fluid>
+        <Jumbotron>
+          <h1>Results</h1>
+          DATE: {this.state.date}
+        </Jumbotron>
+        <Map />
+
+        <Row>
+          <Col size="md-6">
+          Distance of trip: 
+            <Gas lat={30.2672} lng={-97.7431} />
+          </Col>
+          <Col size="md-6 sm-12">
+            <Air />
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
-  
+
 export default Results;
