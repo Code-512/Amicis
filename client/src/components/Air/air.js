450c9d0d0a3c2f3ccc38e90d8af6770c170ba091
Wilmer <julieannewilmer@gmail.com>
2019-01-28 10:07:59 -0600

    commit before checkout new branch

--- client/src/components/Air/air.js
+++ client/src/components/Air/air.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 // import Iata from '../Iata/iata';
 
-
-// import iata from ".//" 
-
-
+// import iata from ".//"
 
 class Air extends React.Component {
   state = {
@@ -13,54 +10,55 @@ class Air extends React.Component {
   };
 
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   componentDidMount() {
-    axios.get('https://api.skypicker.com/flights?flyFrom=AUS&to=BOS&dateFrom=18/02/2019&partner=picky')
+    axios
+      .get(
+        "https://api.skypicker.com/flights?flyFrom=AUS&to=BOS&dateFrom=18/02/2019&partner=picky"
+      )
 
-//  axios.get(`https://api.skypicker.com/flights?flyFrom=AUS&to=AUS&dateFrom=${this.props.date}&partner=picky`)
+      //  axios.get(`https://api.skypicker.com/flights?flyFrom=AUS&to=AUS&dateFrom=${this.props.date}&partner=picky`)
 
-
-    // axios.get(`https://api.skypicker.com/flights?flyFrom=${this.props.orgCode}&to=${this.props.destCode}&dateFrom=${this.props.date}&partner=picky`)
+      // axios.get(`https://api.skypicker.com/flights?flyFrom=${this.props.orgCode}&to=${this.props.destCode}&dateFrom=${this.props.date}&partner=picky`)
 
       .then(response => {
         // let airList = response.data.data
         // return airList.map(item => item.airlines[0] = iata(item.airlines[0]) )
-        return response.data.data
-        
+        return response.data.data;
+
         // return airport code
       })
       .then(airList => {
         this.setState({
           airList
-        })
+        });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
-      })
-
+      });
   }
 
   render() {
-    let { airList } = this.state
+    let { airList } = this.state;
     return (
       <div>
-       {airList.map((item, index) => {
+        {airList.map((item, index) => {
           if (index < 5) {
-            return <div className="margin-bottom-is-20px">
+            return (
+              <div className="margin-bottom-is-20px">
                 <p>Airline: {item.airlines[0]} </p>
                 <p>Flight price: USD ${item.price}</p>
                 <p>Travel Time: {item.fly_duration}</p>
                 <br />
-              </div>;
+              </div>
+            );
           }
         })}
       </div>
-
     );
   }
 }
 
 export default Air;
-
